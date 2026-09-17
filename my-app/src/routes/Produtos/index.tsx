3c8b281f3e5f0caa39868af935a0c3626fb21eb4
isabellearaujod <isabellearaujod@gmail.com>
2026-09-16 21:05:10 -0300

    feat : consumo de api local realizando GET-All com fetch

--- my-app/src/routes/Produtos/index.tsx
+++ my-app/src/routes/Produtos/index.tsx
@@ -1,23 +1,75 @@
 import { useEffect, useState } from "react";
-import { listaProdutos } from "../../data/listaProdutos";
 import type { TipoProduto } from "../../types/types";
 
 export default function Produtos() {
 
+    const [produtos, setProdutos] = useState<TipoProduto[]>([]);
 
-    const[produtos,setProdutos] = useState<TipoProduto[]>([]);
+    //realizando o GetAllProdutos(recebendo toda lista de produtos)
+    useEffect(() => {
 
-    useEffect(()=>{
-        setProdutos(listaProdutos);
-    },[]);
+        async function carregarProdutos() {
+
+            try {
+
+                const response = await fetch("http://localhost:3001/produtos");
+
+                //200 a 299 é True - diferente disso é False
+                if (response.ok) {
+                    throw new Error("Lista de produtos falhou!")
+                }
+
+                const data: TipoProduto[] = await response.json();
+
+                // False = catch
+            } catch (error) {
+                console.error(error);
+            }
+
+        }
+
+        carregarProdutos();
+
+    }, []);
 
     return (
         <main>
             <h2>Produtos</h2>
-            <p>Lista de produtos.</p>
 
-            <ul>
-            </ul>
+            <div>
+                <table border={1}>
+
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NOME</th>
+                            <th>PREÇO</th>
+                            <th>ESTOQUE</th>
+                            <th>AÇÕES</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {produtos.map((produto, indice, array) => (
+                            <tr key={indice}>
+                                <td>{produto.id}</td>
+                                <td>{produto.nome}</td>
+                                <td>{produto.preco}</td>
+                                <td>{produto.estoque}</td>
+                                <td>EDITAR/EXCLUIR</td>
+                            </tr>
+
+                        ))}
+                    </tbody>
+
+                    <tfoot>
+                        <tr> 
+                            <td colSpan={5}>Quantidade de produtos: {produtos?.length}</td>
+                        </tr>
+                    </tfoot>
+
+                </table>
+            </div>
 
         </main>
     );
